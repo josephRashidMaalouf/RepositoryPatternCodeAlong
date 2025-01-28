830017c81e2d1e8d10e68c684059ddde34930171
Joseph Rashid-Maalouf <joseph_maalouf@outlook.com>
2025-01-28 13:04:49 +0100

    .

--- Presentation/Program.cs
+++ Presentation/Program.cs
@@ -2,8 +2,8 @@ using Application.Interfaces;
 using Application.Models;
 using Application.Services;
 using Infrastructure.MongoDb.Repositories;
-using Infrastructure.Persistence;
-using Microsoft.EntityFrameworkCore;
+//using Infrastructure.Persistence;
+//using Microsoft.EntityFrameworkCore;
 using Scalar.AspNetCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -12,11 +12,11 @@ var builder = WebApplication.CreateBuilder(args);
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 
-builder.Services.AddScoped<IPeopleRepository, PeopleMongoRepository>();
 //builder.Services.AddScoped<IPeopleRepository, PeopleRepository>();
+builder.Services.AddScoped<IPeopleRepository, PeopleMongoRepository>();
 builder.Services.AddScoped<IPeopleService, PeoplePeopleService>();
 
-builder.Services.AddDbContext<AppDbContext>(options => options.UseInMemoryDatabase("InMemoryDb"));
+//builder.Services.AddDbContext<AppDbContext>(options => options.UseInMemoryDatabase("InMemoryDb"));
 
 var app = builder.Build();
 
